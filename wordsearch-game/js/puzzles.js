847286ffc9a1ccf275a38a05c67c9d960f746c5f
Shivangi Tikekar <shivz@live.in>
2026-04-17 09:54:56 -0700

    reorder wordsearch puzzle

--- wordsearch-game/js/puzzles.js
+++ wordsearch-game/js/puzzles.js
@@ -45,7 +45,7 @@ const PUZZLES = [
     illustration_asset: null
   },
   {
-    id: "2026-04-05",
+    id: "2026-04-17",
     dish_name: "Guacamole",
     dish_display_name: "Guacamole",
     cuisine: "Mexican",
